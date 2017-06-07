b175d82b4ddb7bf146e2e31c099de34bb9d0fd79
cameronsima <cjsima@gmail.com>
2017-06-07 14:00:54 -0400

    map displayinh

--- src/pages/way-point-map/way-point-map.ts
+++ src/pages/way-point-map/way-point-map.ts
@@ -11,14 +11,37 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 @Component({
   selector: 'page-way-point-map',
   templateUrl: 'way-point-map.html',
+    styles: [`
+    .sebm-google-map-container {
+  height: 300px;
+}
+`
+  ]
 })
 export class WayPointMapPage {
 
+  title: string;
+  lat: number;
+  lng: number;
+  zoom: number = 13;
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.getCurrentLocation();
+  }
+
+  getCurrentLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        this.lat = position.coords.latitude,
+        this.lng = position.coords.longitude
+      })
+    }
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad WayPointMapPage');
   }
+
+
 
 }

fb6a60bb13c30ad44d5720ccee8bdf60d7b73df7
Dan Zygmund-Felt <d.zygmund.felt@gmail.com>
2017-06-10 13:02:26 -0400

    added sample json for destinations and donors, plus made changes to pickup.service

--- src/app/services/pickup.service.ts
+++ src/app/services/pickup.service.ts
@@ -1,6 +1,5 @@
 import {Injectable} from "@angular/core";
 import {Http} from "@angular/http";
-//import { UserService } from ./user.service
 
 @Injectable()
 export class PickupService {
@@ -19,30 +18,40 @@ export class PickupService {
     });
   }
 
-  getPickupsForVolunteer(location: Location) {
-    let self = this;
-    let pickups = [];
-    for(let i = 0; i < this.data.length; i++) {
-      //condition for location being nearby: if yes then
-    }
-    return null;
+  toRadians(num: number) {
+    return num*Math.PI/180;
+  }
+
+  findDistance(lat1: number, lng1: number, lat2: number, lng2: number) {
+    let lat1Rads = this.toRadians(lat1);
+    let lat2Rads = this.toRadians(lat2);
+    let latDiff = this.toRadians(lat2-lat1);
+    let lngDiff = this.toRadians(lng2-lng1);
+    let a = Math.sin(latDiff/2) * Math.sin(latDiff/2) +
+          Math.cos(lat1Rads) * Math.cos(lat2Rads) *
+          Math.sin(lngDiff/2) * Math.sin(lngDiff/2);
+    let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+
+    return c * 3959; //radius of earth = 3959mi
+  }
+
+  getPickupsForVolunteer(lat: number, lng: number) {
+    this.retrieveData();
+    return this.data.filter((item) => item["status"] === "open"
+      && this.findDistance(item["donor"]["location"]["lat"],
+        item["donor"]["location"]["lng"], lat, lng) <= 15)
   }
 
   getPickupByPickupid(pickupid: number) {
-
-    return null;
+    this.retrieveData();
+    return this.data.filter((item) => item["pickupid"] === pickupid);
   }
 
-  getPickupByUserid(userid: number) {
-    return null;
-  }
-
-  getPickupByDonorid(donorid: number) {
-    return null;
-  }
-
-  getPickupByDestinationid(destinationid: number) {
-    return null;
+  getNumberOfPickupsByUserid(userid: number) {
+    //this would be used to get the number of pickups
+    //made by an individual driver so that we can
+    //gamify the app.
+    return 0;
   }
 
 }
